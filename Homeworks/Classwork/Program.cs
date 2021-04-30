56bcb25b16d7b1b7ad08a8460133a8b4badb401b
ali-jafarov <ali.jafarov13@gmail.com>
2021-04-30 12:03:55 +0400

    classwork

---
+++ Homeworks/Classwork/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Classwork
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите сумму наличных");
+
+
+      double cash = Convert.ToDouble(Console.ReadLine());
+
+      
+      
+        if (cash > 14)
+        {
+          Console.WriteLine("invalid input");
+          
+        }
+        
+       
+        
+      
+      
+      
+      double bread = 0.45;
+      double salt = 0.56;
+      double gum = 0.73;
+      double cola = 1.34;
+      double peanut = 1.49;
+      double milk = 2.35;
+      double sigarets = 3.94;
+      double sweets = 5;
+
+      if () 
+      {
+        Console.WriteLine("У вас недостаточно наличных");
+      }
+      
+
+    }
+  }
+}
