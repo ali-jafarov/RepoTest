f677a63bdca4f116340f1cd25c19cc2a0b04cd12
ali-jafarov <ali.jafarov13@gmail.com>
2021-05-27 10:42:19 +0400

    pingpong

---
+++ Homeworks/Ping Pong Events/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ping_Pong_Events
+{
+  class Ping
+  {
+    public event MessageDelegate NotifyEvent;
+
+    public void ReceivePong()
+    {
+      NotifyEvent?.Invoke();
+    }
+  }
+
+  class Pong
+  {
+    public event MessageDelegate NotifyEvent;
+
+    public void ReceivePing()
+    {
+      NotifyEvent?.Invoke();
+    }
+  }
+
+  delegate void MessageDelegate();
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var ping = new Ping();
+      var pong = new Pong();
+
+      ping.NotifyEvent += () => Console.WriteLine("Ping received Pong");
+      pong.NotifyEvent += () => Console.WriteLine("Pong received Ping");
+
+      for (int i = 0; i < 10; i++)
+      {
+        ping.ReceivePong();
+        pong.ReceivePing();
+      }
+    }
+
+  }
+}
